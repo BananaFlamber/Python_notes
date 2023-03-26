5677b73b262d4fde293ccc1703476ff4c85aa32f
BananaFlamber <KorolevSteel@inbox.ru>
2023-03-26 15:27:09 +0300

    Внедрил json

--- main.py
+++ main.py
@@ -1,59 +1,95 @@
-import os
+import json
+from datetime import datetime
+
+FILENAME = 'notes.json'
 
 def main():
     while True:
-        choice = input("Выберите действие: (1) просмотреть заметки, (2) добавить заметку, (3) удалить заметку, (4) выход: ")
-        if choice == "1":
-            show_notes()
-        elif choice == "2":
+        command = input('Введите команду (add, edit, delete, filter, list, exit): ')
+        if command == 'add':
             add_note()
-        elif choice == "3":
+        elif command == 'edit':
+            edit_note()
+        elif command == 'delete':
             delete_note()
-        elif choice == "4":
+        elif command == 'filter':
+            filter_notes()
+        elif command == 'list':
+            print_all_notes()
+        elif command == 'exit':
             break
         else:
             print("Неправильный ввод. Попробуйте еще раз.")
 
-def show_notes():
-    if not os.path.exists("notes.txt"):
-        print("Заметок нет.")
-        return
+def load_notes():
+    try:
+        with open(FILENAME, 'r') as f:
+            notes = json.load(f)
+    except FileNotFoundError:
+        notes = []
+    return notes
 
-    with open("notes.txt", "r") as f:
-        notes = f.readlines()
-
-    if not notes:
-        print("Заметок нет.")
-    else:
-        for i, note in enumerate(notes):
-            print(f"{i+1}. {note.strip()}")
+def save_notes(notes):
+    with open(FILENAME, 'w') as f:
+        json.dump(notes, f)
 
 def add_note():
-    note = input("Введите заметку: ")
-    with open("notes.txt", "a") as f:
-        f.write(f"{note}\n")
-    print("Заметка добавлена.")
+    title = input('Введите заголовок заметки: ')
+    body = input('Введите тело заметки: ')
+    timestamp = datetime.now().isoformat()
+    note = {
+        'id': len(load_notes()) + 1,
+        'title': title,
+        'body': body,
+        'timestamp': timestamp
+    }
+    notes = load_notes()
+    notes.append(note)
+    save_notes(notes)
+    print('Заметка успешно сохранена')
+
+def edit_note():
+    note_id = input('Введите номер заметки, которую нужно отредактировать: ')
+    notes = load_notes()
+    for note in notes:
+        if note['id'] == int(note_id):
+            note['title'] = input('Введите новый заголовок заметки: ')
+            note['body'] = input('Введите новое тело заметки: ')
+            note['timestamp'] = datetime.now().isoformat()
+            save_notes(notes)
+            print('Заметка успешно отредактирована')
+            return
+    print('Заметка не найдена')
 
 def delete_note():
-    show_notes()
-    note_num = input("Введите номер заметки, которую нужно удалить: ")
-    if not note_num.isdigit():
-        print("Неправильный ввод. Попробуйте еще раз.")
+    note_id = input('Введите номер заметки, которую нужно удалить: ')
+    notes = load_notes()
+    for note in notes:
+        if note['id'] == int(note_id):
+            notes.remove(note)
+            save_notes(notes)
+            print('Заметка успешно удалена')
+            return
+    print('Заметка не найдена')
+
+def filter_notes():
+    date_str = input('Введите дату в формате ГГГГ-ММ-ДД: ')
+    try:
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        print('Неверный формат даты')
         return
+    notes = load_notes()
+    filtered_notes = [note for note in notes if datetime.fromisoformat(note['timestamp']).date() == date.date()]
+    print_notes(filtered_notes)
 
-    note_num = int(note_num)
-    with open("notes.txt", "r") as f:
-        notes = f.readlines()
+def print_notes(notes):
+    for note in notes:
+        print(f"{note['id']}. {note['title']}\n{note['body']}\n{note['timestamp']}\n")
 
-    if note_num < 1 or note_num > len(notes):
-        print("Неправильный номер заметки. Попробуйте еще раз.")
-        return
-
-    notes.pop(note_num-1)
-    with open("notes.txt", "w") as f:
-        f.writelines(notes)
-
-    print("Заметка удалена.")
+def print_all_notes():
+    notes = load_notes()
+    print_notes(notes)
 
 if __name__ == "__main__":
     main()
